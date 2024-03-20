6d0d54b70828a3ea48d13005cbd5f1f963d6a4a8
Fayyaad Hassan <sanfayyaad@gmail.com>
2024-03-20 18:55:16 -0400

    Makes call to spotifies API that indicated a succesful login

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,29 +1,24 @@
+import 'dart:convert';
+import 'dart:html' as html; // Import dart:html for web-specific functionality
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:songbird/widgets/form_container_widget.dart';
-
 
 import '../firebase_auth/firebase_auth_class.dart';
 import 'signup.dart';
 
-
 class LoginPage extends StatefulWidget {
-  final Widget? child;
-  const LoginPage({super.key, this.child});
-
+  const LoginPage({Key? key}) : super(key: key);
 
   @override
   State<LoginPage> createState() => _LoginPageState();
 }
 
-
 class _LoginPageState extends State<LoginPage> {
   final FirebaseAuthService _auth = FirebaseAuthService();
-
-
-  TextEditingController _emailController = TextEditingController();
-  TextEditingController _passwordController = TextEditingController();
-
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
 
   @override
   void dispose() {
@@ -32,122 +27,142 @@ class _LoginPageState extends State<LoginPage> {
     super.dispose();
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.grey,//PAGE COLOR
-        body: 
-        Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                  'lib/images/songbird_black_logo_and_text.png',
-                  width: 280, // Adjust width according to your preference
-                  height: 120, // Adjust height according to your preference
-                ),
-              Text(
-                "Login",
-                style: TextStyle(fontSize: 27, fontWeight: FontWeight.bold),
+      backgroundColor: Colors.grey,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'lib/images/songbird_black_logo_and_text.png',
+              width: 280,
+              height: 120,
+            ),
+            Text(
+              'Login',
+              style: TextStyle(fontSize: 27, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 30),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: FormContainerWidget(
+                controller: _emailController,
+                hintText: 'Email',
+                isPasswordField: false,
               ),
-              SizedBox(
-                height: 30,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: FormContainerWidget(
+                controller: _passwordController,
+                hintText: 'Password',
+                isPasswordField: true,
               ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: FormContainerWidget(
-                  controller: _emailController,
-                  hintText: "Email",
-                  isPasswordField: false,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: FormContainerWidget(
-                  controller: _passwordController,
-                  hintText: "Password",
-                  isPasswordField: true,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(2.0),
-                child: GestureDetector(
-                  onTap: _signIn,
-                  child: Container(
-                      width: 250,
-                      height: 45,
-                      decoration: BoxDecoration(
-                        color: Colors.yellow,//LOGIN BUTTON COLOR
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Center(
-                          child: Text(
-                        "Login",
-                        style: TextStyle(
-                            color: Colors.black, fontWeight: FontWeight.bold),
-                      ))),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(2.0),
+              child: GestureDetector(
+                onTap: _signIn,
+                child: Container(
+                  width: 250,
+                  height: 45,
+                  decoration: BoxDecoration(
+                    color: Colors.yellow,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Center(
+                    child: Text(
+                      'Login',
+                      style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+                    ),
+                  ),
                 ),
               ),
-              SizedBox(
-                height: 20,
-              ),
-              Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-                Text("Dont have an account?"),
-                SizedBox(
-                  width: 5,
-                ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _loginWithSpotify,
+              child: Text('Login with Spotify'),
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Don\'t have an account?'),
+                SizedBox(width: 5),
                 GestureDetector(
-                    onTap: () {
-                      Navigator.pushAndRemoveUntil(
-                        context,
-                        MaterialPageRoute(builder: (context) => SignUpPage()),
-                        (route) => false,
-                      );
-                    },
-                    child: Text("Sign Up",
-                        style: TextStyle(
-                          color: Colors.yellow,
-                          fontWeight: FontWeight.bold,
-                        )))
-              ])
-            ],
-          ),
-        ));
+                  onTap: () {
+                    Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(builder: (context) => SignUpPage()),
+                      (route) => false,
+                    );
+                  },
+                  child: Text(
+                    'Sign Up',
+                    style: TextStyle(color: Colors.yellow, fontWeight: FontWeight.bold),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
-
 
   void _signIn() async {
     String email = _emailController.text;
     String password = _passwordController.text;
 
-
+    // Sign in user with Firebase Authentication
     User? user = await _auth.signInWithEmailAndPassword(email, password);
 
-
     if (user != null) {
+      // Navigate to home page
       Navigator.pushNamed(context, "/home");
-    } else { //login error button
+    } else {
+      // Display login error dialog
       showDialog(
         context: context,
         builder: (BuildContext context) {
           return AlertDialog(
-            title: Text("Login Failed", textAlign: TextAlign.center,),
-            content: Text(
-              "The login was unsuccessful. Please check your credentials and try again.",
+            title: Text(
+              'Login Failed',
               textAlign: TextAlign.center,
-              ),
+            ),
+            content: Text(
+              'The login was unsuccessful. Please check your credentials and try again.',
+              textAlign: TextAlign.center,
+            ),
             actions: [
               TextButton(
                 onPressed: () {
                   Navigator.of(context).pop();
                 },
-                child: Text("OK"),
+                child: Text('OK'),
               ),
             ],
           );
         },
       );
     }
+  }
+
+  void _loginWithSpotify() {
+    final clientId = '25c17281eac544f7b24e184c57f9cd0e';
+    final redirectUri = 'https://www.google.com'; // Replace with your redirect URI
+    final List<String> scopes = ['user-read-private', 'user-read-email'];
+
+    final authorizationUrl =
+        'https://accounts.spotify.com/authorize?client_id=$clientId&redirect_uri=$redirectUri&scope=${scopes.join('%20')}&response_type=code';
+
+    // Open Spotify authorization page in a new tab/window
+    html.window.open(authorizationUrl, 'Spotify Login');
+
+    // Print message indicating successful call to Spotify's Web API
+    print('Successfully initiated Spotify login process');
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,10 +9,14 @@ import cloud_firestore
 import firebase_auth
 import firebase_core
 import firebase_storage
+import flutter_web_auth
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
+  FlutterWebAuthPlugin.register(with: registry.registrar(forPlugin: "FlutterWebAuthPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
