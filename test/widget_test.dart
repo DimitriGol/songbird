70d5d5dc6dfdf86530e0c0bb4b5c76affa925a18
Dylan Deokinandan <dylandeo817@gmail.com>
2024-05-19 11:56:36 -0400

    Removed Dummy Buttons

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,7 +10,10 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:songbird/main.dart';
 
+
 void main() {
+
+  
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
